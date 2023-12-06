a2e4b20cb1aae26fc68af7d46ef9cebe3e5d562c
Bishal487 <bishal.kunwar487@gmail.com>
2023-12-05 21:51:38 -0500

    store and model implemented for issue (Bug, story and task)

---
+++ src/com/lambton/store/issue/IssueStoreImpl.java
@@ -0,0 +1,61 @@
+package com.lambton.store.issue;
+
+import com.lambton.common.store.*;
+import com.lambton.enums.issue.*;
+import com.lambton.model.issue.*;
+import com.lambton.utility.*;
+
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.*;
+
+abstract public class IssueStoreImpl<T extends Issue> extends StoreImpl<T> implements IssueStore<T> {
+    public IssueStoreImpl(FileUtility<T> fileUtility) {
+        super(fileUtility);
+    }
+
+    public Stream<T> searchStream(
+            long page,
+            long size,
+            Optional<String> optionalTitle,
+            Optional<String> optionalAssigneeId,
+            Optional<Priority> optionalPriority,
+            Optional<IssueStatus> optionalIssueStatus
+    ) {
+        return fileUtility
+                .readAllEntities()
+                .values()
+                .stream()
+                .skip(page * size)
+                .limit(size)
+                .filter(filterByTitle(optionalTitle))
+                .filter(filterByAssigneeId(optionalAssigneeId))
+                .filter(filterByPriority(optionalPriority))
+                .filter(filterByIssueStatus(optionalIssueStatus));
+    }
+
+    public abstract List<T> search(
+            long page,
+            long size,
+            Optional<String> optionalTitle,
+            Optional<String> optionalAssigneeId,
+            Optional<Priority> optionalPriority,
+            Optional<IssueStatus> optionalIssueStatus
+    );
+
+    private static <T extends Issue> Predicate<T> filterByIssueStatus(Optional<IssueStatus> optionalIssueStatus) {
+        return issue -> optionalIssueStatus.map(issueStatus -> issue.getIssueStatus().equals(issueStatus)).orElse(true);
+    }
+
+    private static <T extends Issue> Predicate<T> filterByPriority(Optional<Priority> optionalPriority) {
+        return issue -> optionalPriority.map(priority -> issue.getPriority().equals(priority)).orElse(true);
+    }
+
+    private static <T extends Issue> Predicate<T> filterByAssigneeId(Optional<String> optionalAssigneeId) {
+        return issue -> optionalAssigneeId.map(assignedId -> issue.getAssignee().getId().equals(assignedId)).orElse(true);
+    }
+
+    private static <T extends Issue> Predicate<T> filterByTitle(Optional<String> optionalTitle) {
+        return issue -> optionalTitle.map(title -> issue.getTitle().toLowerCase().contains(title.toLowerCase())).orElse(true);
+    }
+}
