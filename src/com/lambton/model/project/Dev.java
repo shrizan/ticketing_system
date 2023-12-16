50622abea58eae2a257012aee8b339dd576b62a5
Shreejan Acharya <shreejan.acharya@cnspartner.com>
2023-12-16 07:42:55 -0500

    bug fixed

--- src/com/lambton/model/project/Dev.java
+++
@@ -1,9 +0,0 @@
-package com.lambton.model.project;
-
-import com.lambton.enums.project.ProjectType;
-
-public class Dev extends Project{
-    public Dev(String title, String description) {
-        super(title, description, ProjectType.NEW);
-    }
-}
